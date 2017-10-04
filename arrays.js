041d736cf5ec877f130727de4f8e82efe6e69e7f
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-04 22:57:05 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  return array.unshift(element);
+}
