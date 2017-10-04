f5e31a6034b351d34fb20346ae84102b3cc65697
Rachel Rapoza <rachelrapoza@gmail.com>
2017-10-04 23:18:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,8 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   array.pop();
   return array;
 }
+
+function removeElementFromBeginningOfArray (array) {
+  array.slice(1);
+  return array;
+}
